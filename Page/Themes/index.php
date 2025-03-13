0354b4e74c2a25d81a1a6f5e0cfad51d10a04eeb
azlanajju <muhammedazlan11@gmail.com>
2025-03-13 23:35:49 +0530

    updated and push

--- Page/Themes/index.php
+++ Page/Themes/index.php
@@ -68,159 +68,158 @@
     <!--================ PAGE-TITLE END =================-->
 
 <br>
- <!--================= S-themes =================-->
- <section class="s-schedule-event schedule-event-home-two" id="Events" style="z-index: 0;">
-    <span class="title-position title-position-left">Program</span>
-    <div class="container" ">
-        <h2 data-aos="fade-up" data-aos-delay="50" class="title-line">Themes</h2>
-        <ul class="schedule-cover">
-            
-            <li>
-                <div data-aos="fade-up" data-aos-delay="50" class="schedule-header">
-                    <div class="schedule-time">Theme 1</div>
-                    <div class="schedule-title"> MECH-TECH: INTERNATIONAL CONFERENCE ON EMERGING TRENDS IN MECHANICAL ENGINEERING</div>
-                    <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                </div>
-                <div class="schedule-content">
-                    <ul>
-                        <li> ↪ Design and production</li>
-                        <li> ↪ Thermal sciences</li>
-                        <li> ↪ Manufacturing and machining</li>
-                        <li> ↪ Energy and environment</li>
-                        <li> ↪ Robotics and automation</li>
-                        <li> ↪ Materials and modeling</li>
-                        <li></li> <br>
-                        <li><a class="btn" href="../../themes/mechTech.html">Know more</a> <a style="background:  rgb(224, 3, 3);color: white;" class="btn" href="../../registrations/register1.html">Submit Now</a></li>
-                    </ul>
 
-                </div>
-            </li>
-            <li>
-                <div data-aos="fade-up" data-aos-delay="50" class="schedule-header">
-                    <div class="schedule-time">Theme 2</div>
-                    <div class="schedule-title">IC ICE: INTERNATIONAL CONCLAVE - IN CIVIL ENGINEERING</div>
-                    <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                </div>
-                <div class="schedule-content">
-                    <ul>
-                        <li> ↪ Transportation and Geotechnical Engineering</li>
-                        <li> ↪ Building Materials and Construction Technology</li>
-                        <li> ↪ Architecture and Planning</li>
-                        <li> ↪ Structural Engineering</li>
-                        <li> ↪ Sustainability and Environmental Engineering</li>
-                        <li> ↪ Water Resources and Irrigation Engineering</li>
-                        <li></li><br>
-                        <li><a class="btn" href="../../themes/ic-ice.html">Know more</a> <a style="background:  rgb(224, 3, 3);color: white;" class="btn" href="../../registrations/register2.html">Submit Now</a></li>
-                    </ul>
+    <!--================= S-themes =================-->
+    <section class="s-schedule-event schedule-event-home-two" id="Events">
+        <span class="title-position title-position-left">Program</span>
+        <div class="container">
+            <h2 data-aos="fade-up" data-aos-delay="50" class="title-line">Themes</h2>
+            <ul class="schedule-cover">
+                <li>
+                    <div data-aos="fade-up" data-aos-delay="50" class="schedule-header">
+                        <div class="schedule-time">Theme 1</div>
+                        <div class="schedule-title">MECH-TECH: INTERNATIONAL CONFERENCE ON EMERGING TRENDS IN MECHANICAL ENGINEERING</div>
+                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                    </div>
+                    <div class="schedule-content">
+                        <ul>
+                            <li> ↪ Design and production</li>
+                            <li> ↪ Thermal sciences</li>
+                            <li> ↪ Manufacturing and machining</li>
+                            <li> ↪ Energy and environment</li>
+                            <li> ↪ Robotics and automation</li>
+                            <li> ↪ Materials and modeling</li>
+                            <li></li> <br>
+                            <li><a class="btn" href="./MechTech/">Know more</a> <a style="background:  rgb(224, 3, 3);color: white;" class="btn" href="./MechTech/Submit">Submit Now</a></li>
+                        </ul>
 
-                </div>
-            </li>
-            <li>
-                <div data-aos="fade-up" data-aos-delay="50" class="schedule-header">
-                    <div class="schedule-time">Theme 3</div>
-                    <div class="schedule-title">DIGITAREV 2024: UNVEILING THE NEXT WAVE – INTERNATIONAL CONFERENCE ON <br>DIGITAL RENAISSANCE</div>
-                    <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                </div>
-                <div class="schedule-content">
-                    <ul>
-                        <li> ↪ Data Science and Machine learning</li>
-                        <li> ↪ Cloud and Grid computing</li>
-                        <li> ↪ Blockchain technologies</li>
-                        <li> ↪ Network security & Cryptography</li>
-                        <li> ↪ Computational sciences</li>
-                        <li> ↪ Cyber physical systems</li>
+                    </div>
+                </li>
+                <li>
+                    <div data-aos="fade-up" data-aos-delay="50" class="schedule-header">
+                        <div class="schedule-time">Theme 2</div>
+                        <div class="schedule-title">IC ICE: INTERNATIONAL CONCLAVE - IN CIVIL ENGINEERING</div>
+                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                    </div>
+                    <div class="schedule-content">
+                        <ul>
+                            <li> ↪ Transportation and Geotechnical Engineering</li>
+                            <li> ↪ Building Materials and Construction Technology</li>
+                            <li> ↪ Architecture and Planning</li>
+                            <li> ↪ Structural Engineering</li>
+                            <li> ↪ Sustainability and Environmental Engineering</li>
+                            <li> ↪ Water Resources and Irrigation Engineering</li>
+                            <li></li><br>
+                            <li><a class="btn" href="./ICICE/">Know more</a> <a style="background:  rgb(224, 3, 3);color: white;" class="btn" href="./ICICE/Submit/">Submit Now</a></li>
+                        </ul>
 
-                        <li></li><br>
-                        <li><a class="btn" href="../../themes/DIGITAREV.html">Know more</a> <a style="background:  rgb(224, 3, 3);color: white;" class="btn" href="../../registrations/register3.html">Submit Now</a></li>
-                    </ul>
+                    </div>
+                </li>
+                <li>
+                    <div data-aos="fade-up" data-aos-delay="50" class="schedule-header">
+                        <div class="schedule-time">Theme 3</div>
+                        <div class="schedule-title">DIGITAREV 2025: UNVEILING THE NEXT WAVE – INTERNATIONAL CONFERENCE ON <br>DIGITAL RENAISSANCE</div>
+                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                    </div>
+                    <div class="schedule-content">
+                        <ul>
 
-                </div>
-            </li>
-            <li>
-                <div data-aos="fade-up" data-aos-delay="50" class="schedule-header">
-                    <div class="schedule-time">Theme 4</div>
-                    <div class="schedule-title">SEMI-COMM TECH SUMMIT: INTERNATIONAL CONFERENCE ON EMERGING TRENDS IN ELECTRONICS AND COMMUNICATION</div>
-                    <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                </div>
-                <div class="schedule-content">
-                    <ul>
-                        <li> ↪ Wireless Communication</li>
-                        <li> ↪ Embedded System</li>
-                        <li> ↪ Signal and Image Processing</li>
-                        <li> ↪ Electromagnetics and Antenna Design</li>
-                        <li> ↪ VLSI and MEMS</li>
-                        <li> ↪ Communication networks and Security</li>
+                            <li> ↪ Data Science and Machine learning</li>
+                            <li> ↪ Cloud and Grid computing</li>
+                            <li> ↪ Blockchain technologies</li>
+                            <li> ↪ Network security & Cryptography</li>
+                            <li> ↪ Computational sciences</li>
+                            <li> ↪ Cyber physical systems</li>
 
+                            <li></li><br>
+                            <li><a class="btn" href="./Digitarev/">Know more</a> <a style="background:  rgb(224, 3, 3);color: white;" class="btn" href="./Digitarev/Submit/">Submit Now</a></li>
+                        </ul>
 
-                        <li></li><br>
-                        <li><a class="btn" href="../../themes/semiCommTechSummit.html">Know more</a> <a style="background:  rgb(224, 3, 3);color: white;" class="btn" href="../../registrations/register4.html">Submit Now</a></li>
-                    </ul>
+                    </div>
+                </li>
+                <li>
+                    <div data-aos="fade-up" data-aos-delay="50" class="schedule-header">
+                        <div class="schedule-time">Theme 4</div>
+                        <div class="schedule-title">SEMI-COMM TECH SUMMIT: INTERNATIONAL CONFERENCE ON EMERGING TRENDS IN ELECTRONICS AND COMMUNICATION</div>
+                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                    </div>
+                    <div class="schedule-content">
+                        <ul>
+                            <li> ↪ Wireless Communication</li>
+                            <li> ↪ Embedded System</li>
+                            <li> ↪ Signal and Image Processing</li>
+                            <li> ↪ Electromagnetics and Antenna Design</li>
+                            <li> ↪ VLSI and MEMS</li>
+                            <li> ↪ Communication networks and Security</li>
+                            <li></li><br>
+                            <li><a class="btn" href="./SCTS/">Know more</a> <a style="background:  rgb(224, 3, 3);color: white;" class="btn" href="./SCTS//Submit/">Submit Now</a></li>
+                        </ul>
 
-                </div>
-            </li>
-            <li>
-                <div data-aos="fade-up" data-aos-delay="50" class="schedule-header">
-                    <div class="schedule-time">Theme 5</div>
-                    <div class="schedule-title">BioTrendCon 2024: International Conference on Emerging Trends in Biotechnology</div>
-                    <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                </div>
-                <div class="schedule-content">
-                    <ul>
-                        <li> ↪ Food technology</li>
-                        <li> ↪ Drug design & Nanobiotechnology</li>
-                        <li> ↪ Agricultural biotechnology</li>
-                        <li> ↪ Clinical research</li>
-                        <li> ↪ Environmental engineering</li>
-                        <li> ↪ Bioprocessing</li>
-                        <li></li><br>
-                        <li><a class="btn" href="../../themes/BIOTRENDCON.html">Know more</a> <a style="background:  rgb(224, 3, 3);color: white;" class="btn" href="../../registrations/register5.html">Submit Now</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li>
-                <div class="schedule-header">
-                    <div class="schedule-time">Theme 6</div>
-                    <div class="schedule-title">ARTELLIGENCE 2025:  INTERNATIONAL CONFERENCE CONFERENCE ON ARTIFICIAL INTELLIGENCE</div>
-                    <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                </div>
-                <div class="schedule-content">
-                    <ul>
-                        <li> ↪ Innovation in AI, Machine learning, and Data science</li>
-                        <li> ↪ Intelligent video analytics, Computer vision and Signal processing</li>
-                        <li> ↪ AI and IoT integration in Smart Systems</li>
-                        <li> ↪ AI in Robotics, Autonomous Systems, and Healthcare</li>
-                        <li> ↪ AI for Sustainable technologies and Responsible ML</li>
-                        <li> ↪ Natural Language Processing, Ai Communication, and Ethics</li>
-                        <li></li><br>
-                        <li><a class="btn" href="../../themes/icibs.html">Know more</a> <a style="background: rgb(170, 0, 0);color: white;" class="btn" href="../../registrations/register6.html">Submit Now</a></li>
-                    </ul>
+                    </div>
+                </li>
+                <li>
+                    <div data-aos="fade-up" data-aos-delay="50" class="schedule-header">
+                        <div class="schedule-time">Theme 5</div>
+                        <div class="schedule-title">BIOTRENDCON 2025: INTERNATIONAL CONFERENCE ON EMERGING TRENDS IN BIOTECHNOLOGY</div>
+                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                    </div>
+                    <div class="schedule-content">
+                        <ul>
+                            <li> ↪ Food technology</li>
+                            <li> ↪ Drug design & Nanobiotechnology</li>
+                            <li> ↪ Agricultural biotechnology</li>
+                            <li> ↪ Clinical research</li>
+                            <li> ↪ Environmental engineering</li>
+                            <li> ↪ Bioprocessing</li>
+                            <li></li><br>
+                            <li><a class="btn" href="./Biotrendcon/">Know more</a> <a style="background:  rgb(224, 3, 3);color: white;" class="btn" href="./Biotrendcon/Submit/">Submit Now</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <div class="schedule-header">
+                        <div class="schedule-time">Theme 6</div>
+                        <div class="schedule-title">ARTELLIGENCE 2025: INTERNATIONAL CONFERENCE CONFERENCE ON ARTIFICIAL INTELLIGENCE</div>
+                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                    </div>
+                    <div class="schedule-content">
+                        <ul>
+                            <li> ↪ Innovation in AI, Machine learning, and Data science</li>
+                            <li> ↪ Intelligent video analytics, Computer vision and Signal processing</li>
+                            <li> ↪ AI and IoT integration in Smart Systems</li>
+                            <li> ↪ AI in Robotics, Autonomous Systems, and Healthcare</li>
+                            <li> ↪ AI for Sustainable technologies and Responsible ML</li>
+                            <li> ↪ Natural Language Processing, Ai Communication, and Ethics</li>
+                            <li></li><br>
+                            <li><a class="btn" href="./Artelligence/">Know more</a> <a style="background: rgb(170, 0, 0);color: white;" class="btn" href="./Artelligence/Submit/">Submit Now</a></li>
+                        </ul>
 
-                </div>
-            </li>
-            <li>
-                <div class="schedule-header">
-                    <div class="schedule-time">Theme 7</div>
-                    <div class="schedule-title">ICIBS 2025:  INTERNATIONAL CONFERENCE IN BASIC & APPLIED SCIENCES</div>
-                    <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                </div>
-                <div class="schedule-content">
-                    <ul>
-                        <li> ↪ Chemical sciences</li>
-                        <li> ↪ Materials chemistry and polymers</li>
-                        <li> ↪ Discrete Mathematics</li>
-                        <li> ↪ Applied Science Mathematics</li>
-                        <li> ↪ Nanotechnology</li>
-                        <li> ↪ Applied Physics</li>
-                        <li></li><br>
-                        <li><a class="btn" href="../../themes/icibs.html">Know more</a> <a style="background: rgb(170, 0, 0);color: white;" class="btn" href="../../registrations/register6.html">Submit Now</a></li>
-                    </ul>
+                    </div>
+                </li>
+                <li>
+                    <div class="schedule-header">
+                        <div class="schedule-time">Theme 7</div>
+                        <div class="schedule-title">ICIBS 2025: INTERNATIONAL CONFERENCE IN BASIC & APPLIED SCIENCES</div>
+                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                    </div>
+                    <div class="schedule-content">
+                        <ul>
+                            <li> ↪ Chemical sciences</li>
+                            <li> ↪ Materials chemistry and polymers</li>
+                            <li> ↪ Discrete Mathematics</li>
+                            <li> ↪ Applied Science Mathematics</li>
+                            <li> ↪ Nanotechnology</li>
+                            <li> ↪ Applied Physics</li>
+                            <li></li><br>
+                            <li><a class="btn" href="./ICIBS/">Know more</a> <a style="background: rgb(170, 0, 0);color: white;" class="btn" href="./ICIBS/Submit/">Submit Now</a></li>
+                        </ul>
 
-                </div>
-            </li>
-        </ul>
-    </div>
-</section>
-<!--=============== S-themes END ===============-->
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!--=============== S-themes END ===============-->
 
 
     <!--===================== FOOTER =====================-->
