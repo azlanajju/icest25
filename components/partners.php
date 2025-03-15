4dae70098b38a4e685dd04b24289ea5d2e51d501
azlanajju <muhammedazlan11@gmail.com>
2025-03-15 11:33:13 +0530

    updated partners

---
+++ components/partners.php
@@ -0,0 +1,18 @@
+    <!--===================== S-association =====================-->
+    <div class="container">
+        <section class=" s-title-bg">
+            <span class="title-bg">Partners</span>
+            <div class="container">
+                <h2 data-aos="fade-up" data-aos-delay="50" class="title-line">Our Partner</h2>
+                <div class="">
+                    <div class="partImages" style="display:flex;justify-content:center">
+                        <img data-aos="fade-right" data-aos-delay="50" height="100" src="<?php echo $headPath; ?>images/logos/viganUniversity.png" alt="">
+                        <!--  <img data-aos="fade-right" data-aos-delay="100" height="100" src="./images/logos/westlondon.png" alt="">-->
+
+                    </div>
+                </div>
+       
+            </div>
+        </section>
+
+    </div>
