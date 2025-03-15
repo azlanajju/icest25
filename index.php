55aa1d571e7ec3c4ad63c045121619aa03b0a2ac
azlanajju <muhammedazlan11@gmail.com>
2025-03-15 11:30:23 +0530

    updated partners

--- index.php
+++ index.php
@@ -448,7 +448,7 @@
                 <h2 data-aos="fade-up" data-aos-delay="50" class="title-line">Our Partner</h2>
                 <div class="">
                     <div class="partImages" style="display:flex;justify-content:center">
-                        <img data-aos="fade-right" data-aos-delay="50" height="100" src="./images/logos/presidecy-university.png" alt="">
+                        <img data-aos="fade-right" data-aos-delay="50" height="100" src="./images/logos/viganUniversity.png" alt="">
                         <!--  <img data-aos="fade-right" data-aos-delay="100" height="100" src="./images/logos/westlondon.png" alt="">-->
 
                     </div>
